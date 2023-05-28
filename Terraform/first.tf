3f8a6797480779256f2f719be360b3c1763bc8c0
Avaneesh <avaneeshsingh1999@gmail.com>
2023-05-28 13:32:38 +0530

    Terrafrom refering doc

--- Terraform/first.tf
+++ Terraform/first.tf
@@ -1,3 +1,18 @@
-output heelo1 {
-    value="I am in" 
+# Configure the AWS provider
+provider "aws" {
+  region = "ap-south-1"  # Update with your desired AWS region
 }
+
+# Create a new EC2 instance
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0607784b46cbe5816"  # Update with your desired AMI ID
+  instance_type = "t2.micro"  # Update with your desired instance type
+
+  key_name      = "web-1"  # Update with your desired key pair name
+
+  tags = {
+    Name = "first-resource"
+  }
+}
+
+
