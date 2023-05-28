bfe2982354756d0d2e52fa1df88d9ddd2ebe36c1
Avaneesh <avaneeshsingh1999@gmail.com>
2023-05-28 13:47:57 +0530

    hi

--- Terraform/first.tf
+++ Terraform/first.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "example_instance" {
   ami           = "ami-0607784b46cbe5816"  # Update with your desired AMI ID
   instance_type = "t2.micro"  # Update with your desired instance type
 
-  key_name      = "web-1"  # Update with your desired key pair name
+  key_name      = "web-1"  # Update  with your desired key pair name
 
   tags = {
     Name = "first-resource"
