19c8983d4c3bd8882567d91414f37eccfe915606
Avaneesh <avaneeshsingh1999@gmail.com>
2023-06-01 10:44:40 +0530

    system variables

---
+++ Linux & shell scripting/References.txt/Exercises/firstshellscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is how a comment is made in shell script
+
+echo "Hello This is me"
+echo "Hello\
+   i\
+   am\
+   Avaneesh."
+ 
---
+++ Linux & shell scripting/References.txt/Exercises/variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# user defined variables
+ name=Avaneesh
+ Age=20
+
+ echo "My ${name} is name and ${Age} is age."
